31c69ee8bc6dc615418e8639928bcc758a2d136b
Chan <lhchan4010@gmail.com>
2020-10-25 04:33:21 +0900

    toggleLike

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -4,11 +4,9 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     requestSecret: async (_, args, { request }) => {
-      console.log(request.user);
       const { email } = args;
       const loginSecret = secretGenerator();
       try {
-        throw Error();
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
