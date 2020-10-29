4d5efdd49aca46ea9061e3e2df18b8531f3c3d88
Chan <lhchan4010@gmail.com>
2020-10-29 17:52:57 +0900

    isLiked

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -31,4 +31,24 @@ export default {
       return user.id === parentId;
     },
   },
+  Post: {
+    isLiked: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id,
+            },
+          },
+          {
+            post: {
+              id,
+            },
+          },
+        ],
+      });
+    },
+  },
 };
